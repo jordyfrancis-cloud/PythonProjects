42727de49f0959c561062b21b7123289b32637de
jordyfranciss <156683010+jordyfranciss@users.noreply.github.com>
2026-06-01 22:15:47 -0400

    Second

---
+++ temp.py
@@ -0,0 +1,7 @@
+glass1="Milk"
+glass2="Juice"
+
+temp=glass1
+glass1=glass2
+glass2=temp
+print(glass1+glass2)
---
+++ type.py
@@ -0,0 +1,3 @@
+name=input("Write your name")
+name_length=len(name.replace(" ", ""))
+print(name_length)
